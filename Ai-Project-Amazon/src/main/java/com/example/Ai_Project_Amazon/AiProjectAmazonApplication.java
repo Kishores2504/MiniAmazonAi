4a82f6d992c48837b0acce28d99c50ec7b9f284c
Kishores2504 <kishore.s2504@gmail.com>
2025-11-12 09:21:07 +0530

    "Initial Commit"

---
+++ Ai-Project-Amazon/src/main/java/com/example/Ai_Project_Amazon/AiProjectAmazonApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ai_Project_Amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjectAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProjectAmazonApplication.class, args);
+	}
+
+}
---
+++ Ai-Project-Amazon/src/test/java/com/example/Ai_Project_Amazon/AiProjectAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ai_Project_Amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectAmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
